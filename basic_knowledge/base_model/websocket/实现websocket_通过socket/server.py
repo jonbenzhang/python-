d19932119d66e49cbaf8adbed4247e193c66f636
zhangmeng <15732132531@163.com>
2020-09-02 17:29:24 +0800

    添加websocket 的使用代码

---
+++ basic_knowledge/base_model/websocket/实现websocket_通过socket/server.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/2 上午10:22
+# @Author  : dataport
+# @File    : server.py
+# @Software: PyCharm
---
+++ basic_knowledge/base_model/websocket/简单实现/first.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/2 上午9:46
+# @Author  : dataport
+# @File    : first.py
+# @Software: PyCharm
