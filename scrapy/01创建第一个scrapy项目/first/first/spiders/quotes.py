b91eae6a9c6d7ce4a9c95633a25a0d3454b13469
jenbonzhang <15732132531@163.com>
2020-09-08 23:37:27 +0800

    t

--- scrapy/01创建第一个scrapy项目/first/first/spiders/quotes.py
+++ scrapy/01创建第一个scrapy项目/first/first/spiders/quotes.py
@@ -1,5 +1,7 @@
 import scrapy
 from first.items import QuotesItem
+from scrapy.http import Request
+
 
 class ChoutiSpider(scrapy.Spider):
     name = 'quotes'
@@ -9,7 +11,29 @@ class ChoutiSpider(scrapy.Spider):
     # 起始url
     start_urls = ['http://quotes.toscrape.com/page/1/']
 
+    def start_requests(self):
+        """
+        类,首先执行start_requests,返回一个可迭代对象
+        :return:
+        """
+        # 方式1
+        for url in self.start_urls:
+            yield Request(url, dont_filter=True)
+    #     # # 方式2
+        # req_list = []
+        # for url in self.start_urls:
+        #     req_list.append(Request(url=url))
+        # return req_list
+
     def parse(self, response):
+        # response就是　下面倒入的HtmlResponse类的实例化对象
+        from scrapy.http.response.html import HtmlResponse
+        # 爬取深度控制的中间键
+        from scrapy.spidermiddlewares.depth import DepthMiddleware
+        from scrapy.http import Response
+        # 获取爬取的深度
+        # response.meta = response.request.meta
+        # print(response.meta.get("depth",0))
         div_list = response.xpath("/html/body/div/div[2]/div[1]/div[@class='quote']")
         for div in div_list:
             # .//前面加.在当前标签的子孙标签中寻找
@@ -24,7 +48,6 @@ class ChoutiSpider(scrapy.Spider):
         print(next_page_url)
         # 进行翻页
         for next_page_url in next_page_url:
-            from scrapy.http import Request
             url = "http://quotes.toscrape.com" + next_page_url
             print(url)
             # 爬取过的网页不会再次进行爬取
